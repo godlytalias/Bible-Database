3f2103658985c2bd9038bc14dbc83d38a8465d02
Godly T Alias <godlytalias@yahoo.co.in>
2016-02-12 10:43:38 +0530

    [Bible] Added book lists
    Signed-off-by: Godly T Alias <godlytalias@yahoo.co.in>

---
+++ English/books.h
@@ -0,0 +1,11 @@
+Books[] = {
+						"Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua",
+						"Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles",
+						"2 Chronicles", "Ezra", "Nehemiah", "Esther", "Job", "Psalms", "Proverbs", "Ecclesiastes",
+						"Song of Solomon", "Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Daniel",
+						"Hosea", "Joel", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah",
+						"Haggai", "Zechariah", "Malachi", "Matthew", "Mark", "Luke", "John", "Acts", "Romans",
+						"1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians",
+						"1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus", "Philemon",
+						"Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude", "Revelation"};
+
